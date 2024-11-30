3fdfbe04da15e15b6e55e4e8c69286f6c1d5a508
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-11-30 20:27:46 +0900

    번호 표시 기준 변경

--- lua/options.lua
+++ lua/options.lua
@@ -5,21 +5,5 @@ require "nvchad.options"
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
 
--- local client = vim.lsp.start_client({
--- 	name = "educationalsp",
--- 	cmd = { "/Users/kimsanghyun/Desktop/mainDir/2410/educationalsp" },
---     -- 아래는 개인적인 설정
--- 	-- on_attach = require("plugins.lsp").on_attach,
--- })
---
--- if not client then
--- 	vim.notify("hey, you didnt do the client thing good")
--- 	return
--- end
---
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = "markdown",
--- 	callback = function()
--- 		vim.lsp.buf_acttach_client(0, client)
--- 	end,
--- })
+-- 현재 줄기준으로 표시
+vim.wo.relativenumber = true
